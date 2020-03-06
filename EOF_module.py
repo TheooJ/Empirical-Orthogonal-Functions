dcfef403e5a145902c939134ea36633ef1a1a158
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2020-03-05 19:28:52 -0500

    Make the module more flexible. Combined real and complex computing in the same function. Changed docstring under functions

--- EOF_module.py
+++ EOF_module.py
@@ -1,89 +1,94 @@
 import numpy as np
 
 
-"""Module for Empirical Orthogonal Functions, return the filter.
-Functions are:
-    filter_training_real: for computing of filter with real data
-    filter_training_complex: for computing of filter with complex data
+"""Module for Empirical Orthogonal Functions.
 
-:param l: Number of history vectors in data matrix
-:param m: Number of measurements in history vectors 
-:param n: Number of time samples in history vectors 
-:param delta_t: Delay between the phenomenom happening and acquisition
-:param window: Size of the window of the moving average
-:param alpha: Tiknhonov regularization of the filter
+Functions are:
+    filter_training: compute the filter with real data , return the filter
 """
 
 
 
-def filter_training_real(l,m,n,alpha,data_matrix,a_posteriori_matrix):
+def filter_training(l,m,n,alpha,data_matrix,a_posteriori_matrix,data_type='complex'):
+    """Function to compute filter using EOF.
     
-    #Computing of the filter
-    if alpha == 0:
-        U, sing, VT = np.linalg.svd(data_matrix.T)
-        epsilon = np.zeros((l, n*m), dtype=complex) 
-        epsilon[:min(n*m,l), :min(n*m,l)] = np.diag(sing)
+    :param l: Number of history vectors in data matrix
+    :param m: Number of measurements in history vectors 
+    :param n: Number of time samples in history vectors 
+    :param delta_t: Delay between the phenomenom happening and acquisition
+    :param alpha: Tikhonov regularization of the filter
+    :param data_type: 
         
-        epsilon_pseudo_invert = np.linalg.pinv(epsilon)
+    Return the filter to estimate every element in history vector
+    """
     
-        product = U.dot( epsilon_pseudo_invert.T ).dot( VT )
-        filter_full = a_posteriori_matrix.dot( product )    
-        
-    else:
-        data_matrix_reg = np.c_[ data_matrix, np.sqrt(alpha)*np.eye(n*m) ]
-        a_posteriori_matrix_reg = np.c_[ a_posteriori_matrix, np.zeros([m,n*m]) ]
-        
-        U, sing, VT = np.linalg.svd(data_matrix_reg.T)
-        epsilon = np.zeros((l+n*m, n*m), dtype=complex) 
-        epsilon[:min(n*m,l+n*m), :min(n*m,l+n*m)] = np.diag(sing[:min(n*m,l+n*m)])
-        
-        epsilon_pseudo_invert = np.linalg.pinv(epsilon)
+    if data_type == 'complex': 
+    #Compute filter with complex data
     
-        product = U.dot( epsilon_pseudo_invert.T ).dot( VT )
-        filter_full = a_posteriori_matrix_reg.dot( product )
-
-
-    return filter_full
-
-
-
-
-
-def filter_training_complex(l,m,n,alpha,data_matrix,a_posteriori_matrix):
+        if alpha == 0:
+            U, sing, VT = np.linalg.svd(data_matrix.conj().T)
+            epsilon = np.zeros((l, n*m), dtype=complex) 
+            epsilon[:min(n*m,l), :min(n*m,l)] = np.diag(sing)
+            
+            epsilon_pseudo_invert = np.linalg.pinv(epsilon)
+        
+            product = U.dot( epsilon_pseudo_invert.conj().T ).dot( VT )
+            filter_full = a_posteriori_matrix.dot( product )    
+            
+        else:
+            data_matrix_reg = np.c_[ data_matrix, np.sqrt(alpha)*np.eye(n*m) ]
+            a_posteriori_matrix_reg = np.c_[ a_posteriori_matrix, np.zeros([m,n*m]) ]
+            
+            U, sing, VT = np.linalg.svd(data_matrix_reg.conj().T)
+            epsilon = np.zeros((l+n*m, n*m), dtype=complex) 
+            epsilon[:min(n*m,l+n*m), :min(n*m,l+n*m)] = np.diag(sing[:min(n*m,l+n*m)])
+            
+            epsilon_pseudo_invert = np.linalg.pinv(epsilon)
+        
+            product = U.dot( epsilon_pseudo_invert.conj().T ).dot( VT )
+            filter_full = a_posteriori_matrix_reg.dot( product )
     
-    #Computing of the filter
-    if alpha == 0:
-        U, sing, VT = np.linalg.svd(data_matrix.conj().T)
-        epsilon = np.zeros((l, n*m), dtype=complex) 
-        epsilon[:min(n*m,l), :min(n*m,l)] = np.diag(sing)
-        
-        epsilon_pseudo_invert = np.linalg.pinv(epsilon)
     
-        product = U.dot( epsilon_pseudo_invert.conj().T ).dot( VT )
-        filter_full = a_posteriori_matrix.dot( product )    
+        return filter_full
         
-    else:
-        data_matrix_reg = np.c_[ data_matrix, np.sqrt(alpha)*np.eye(n*m) ]
-        a_posteriori_matrix_reg = np.c_[ a_posteriori_matrix, np.zeros([m,n*m]) ]
+    elif data_type == 'real':
+    #Compute filter with real data
         
-        U, sing, VT = np.linalg.svd(data_matrix_reg.conj().T)
-        epsilon = np.zeros((l+n*m, n*m), dtype=complex) 
-        epsilon[:min(n*m,l+n*m), :min(n*m,l+n*m)] = np.diag(sing[:min(n*m,l+n*m)])
+        if alpha == 0:
+            U, sing, VT = np.linalg.svd(data_matrix.T)
+            epsilon = np.zeros((l, n*m)) 
+            epsilon[:min(n*m,l), :min(n*m,l)] = np.diag(sing)
+            
+            epsilon_pseudo_invert = np.linalg.pinv(epsilon)
         
-        epsilon_pseudo_invert = np.linalg.pinv(epsilon)
+            product = U.dot( epsilon_pseudo_invert.T ).dot( VT )
+            filter_full = a_posteriori_matrix.dot( product )    
+            
+        else:
+            data_matrix_reg = np.c_[ data_matrix, np.sqrt(alpha)*np.eye(n*m) ]
+            a_posteriori_matrix_reg = np.c_[ a_posteriori_matrix, np.zeros([m,n*m]) ]
+            
+            U, sing, VT = np.linalg.svd(data_matrix_reg.T)
+            epsilon = np.zeros((l+n*m, n*m)) 
+            epsilon[:min(n*m,l+n*m), :min(n*m,l+n*m)] = np.diag(sing[:min(n*m,l+n*m)])
+            
+            epsilon_pseudo_invert = np.linalg.pinv(epsilon)
+        
+            product = U.dot( epsilon_pseudo_invert.T ).dot( VT )
+            filter_full = a_posteriori_matrix_reg.dot( product )
     
-        product = U.dot( epsilon_pseudo_invert.conj().T ).dot( VT )
-        filter_full = a_posteriori_matrix_reg.dot( product )
-
-
-    return filter_full
+    
+        return filter_full
 
 
 
 
+
+
+#TO DO: "smart" moving average with rank-one update
 
 def filter_training_moving_average(l,m,n,alpha,window,data_matrix,history_new,a_posteriori_matrix,a_posteriori_new):
-    """ function that returns the history vectors, the data matrix, 
+    """ Function that returns the history vectors, the data matrix, 
     the a posteriori matrix and the full-order filter for the training set """
     
     ##Data matrix update with new history
