ed7095d7d26da01dc5feeb8baf3c7cbdf954ab57
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2020-03-10 00:01:16 -0400

    Added dark hole creation, phase drift on EField, Image averaging, EFC loop for DH maintenance

--- efc_with_EOF.py
+++ efc_with_EOF.py
@@ -35,19 +35,6 @@ offset = 1
 
 efc_loop_gain = 1
 
-#Filter parameters initialization
-l = 3 
-#m = 4   #Chosen later
-n = 2 
-delta_t = 1 
-alpha = 1e-3 
-if alpha < 0:
-  raise Exception("Sorry, regularization parameter must be positive of null") 
-window = 2
-already_computed = False 
-moving_average = 0
-nb_hist = 0 
-
 
 
 # Create grids
@@ -116,15 +103,21 @@ for i, mode in enumerate(np.eye(len(influence_functions))):
 response_matrix = np.array(responses).T
 efc_matrix = inverse_tikhonov(response_matrix, 1e-3)
 
-
-
-# Run EFC loop
 current_actuators = np.zeros(len(influence_functions))
-est_actuators = np.zeros(len(influence_functions))
 
-#m = sum(dark_zone) #Estimate E for every pixel in dark zone
-m = len(current_actuators)
 
+#Filter parameters initialization
+l = 3 
+m = sum(dark_zone) #Estimate E for every pixel in dark zone
+n = 2 
+delta_t = 1 
+alpha = 1e-3 
+if alpha < 0:
+  raise Exception("Sorry, regularization parameter must be positive of null") 
+window = 2
+already_computed = False 
+moving_average = 0
+nb_hist = 0 
 
 #Filter lists initialization (after grids because we need the size of dark_zone/len actuators)
 data_matrix = np.zeros([m*n,l])
@@ -135,78 +128,134 @@ E_measured_list = []
 E_hat_list = []
 MSE_list = []
 rms_diff_list=[]
+est_actuators = np.zeros(sum(dark_zone))
 
 
-for i in range(200):
+
+#Dark hole creation USING NON ACCESSIBLE PERFECT ELECTRIC FIELD (negligeable noise regime)
+while ( sum(np.log10(get_intensity(current_actuators)[dark_zone]))/sum(dark_zone) > -10 ):
     E = get_electric_field(current_actuators)[dark_zone]
     x = np.concatenate((E.real, E.imag))
     
     y = efc_matrix.dot(x)
     current_actuators -= efc_loop_gain * y
-    
-    E = current_actuators
-    
-    #Run EOF loop on E (data_type = 'complex') or on actuators (data_type = 'real') ????
-    E_measured_list.insert(0,E.copy())
-    history_current = np.concatenate((E.copy().flatten(),history_current),axis=0)
-        
-    #Construction of history vectors
-    if len(history_current) == m*n:
-        history_list.insert(0,history_current.copy())
-        history_current = np.delete(history_current,range(len(history_current)-m,len(history_current)))
-        nb_hist +=1
-        
-    #Check if we need to recompute the filter
-    if window is not None: 
-        if moving_average == window: 
-            already_computed = False
-            moving_average = 0    
-    
-    #Construction of the data matrix, a posteriori matrix and computing of the filter  
-    if already_computed is False and nb_hist >= l+delta_t:
-        for i in range(l):
-            data_matrix[:,i] = history_list[i+delta_t]
-            a_posteriori_matrix[:,i] = E_measured_list[i].flatten()
-        F = eof.filter_training(l,m,n,alpha,data_matrix,a_posteriori_matrix,data_type='real') 
-        already_computed = True
-    
-    #Estimation of E if the filter has been computed
-    if already_computed is True:
-        E_hat = F.dot(history_list[0])#.reshape(grid_size,grid_size)
-        E_hat_list.insert(0,E_hat.copy())
-        moving_average += 1
-    
-    
+    	
     img = get_intensity(current_actuators)
     
-    if already_computed:
-        
-        E = get_electric_field(E_hat)[dark_zone]
-        x = np.concatenate((E.real, E.imag))
+    plt.clf()
+    imshow_field(np.log10(img), vmin=-10, vmax=-5, cmap='inferno')
+    plt.title('Dark hole creation')
+    plt.colorbar()
+    plt.draw()
+    plt.pause(0.1)
     
-        y = efc_matrix.dot(x)
-        est_actuators -= efc_loop_gain * y
-        est = get_intensity(est_actuators)
+
+    
+#Initialize drift parameters
+random_walk = np.zeros([sum(dark_zone)])
+E_avg = np.zeros([sum(dark_zone)], dtype=complex)
+observation_time=20
+count = 0
+
+#Dark hole maintenance with drifting phase on electric field
+for i in range(200):
+    if i < observation_time*(count+1):
+        E = get_electric_field(current_actuators)[dark_zone] #Not accessible IRL
+        random_walk += 0.03*np.random.normal(size=([sum(dark_zone)]))
+        E *= np.exp(1j * random_walk)
         
-        plt.clf()
-        plt.subplot(121)
-        imshow_field(np.log10(img), vmin=-10, vmax=-5, cmap='inferno')
-        plt.colorbar()
-        plt.title('Without estimation')
-        plt.draw()
-        
-        plt.subplot(122)
-        imshow_field(np.log10(est), vmin=-10, vmax=-5, cmap='inferno')
-        plt.colorbar()
-        plt.title('With estimation')
-        plt.draw()
-        plt.pause(0.1)
+        #Averaging over the time frames
+        E_avg += E
     
     else:
+        x = np.concatenate((E_avg.real/observation_time, E_avg.imag/observation_time))
+        y = efc_matrix.dot(x)
+        current_actuators -= efc_loop_gain * y
+        img = get_intensity(current_actuators)
+            
         plt.clf()
-        plt.subplot(121)
         imshow_field(np.log10(img), vmin=-10, vmax=-5, cmap='inferno')
+        plt.title('Dark hole maintenance with additive drift, images averaged on {} frames'.format(observation_time))
         plt.colorbar()
-        plt.title('Without estimation')
         plt.draw()
         plt.pause(0.1)
+        
+        E_avg = np.zeros([sum(dark_zone)], dtype=complex)
+        count +=1
+
+    
+#    
+##Drifting dark hole maintenance with Pairwise and extrapolation with EFC
+#for i in range(200):
+#    E = get_electric_field(current_actuators)[dark_zone]
+#    x = np.concatenate((E.real, E.imag))
+#    
+#    y = efc_matrix.dot(x)
+#    current_actuators -= efc_loop_gain * y
+#    
+#    E = current_actuators
+#    
+#    #Run EOF loop on E (data_type = 'complex') or on actuators (data_type = 'real') ????
+#    E_measured_list.insert(0,E.copy())
+#    history_current = np.concatenate((E.copy().flatten(),history_current),axis=0)
+#        
+#    #Construction of history vectors
+#    if len(history_current) == m*n:
+#        history_list.insert(0,history_current.copy())
+#        history_current = np.delete(history_current,range(len(history_current)-m,len(history_current)))
+#        nb_hist +=1
+#        
+#    #Check if we need to recompute the filter
+#    if window is not None: 
+#        if moving_average == window: 
+#            already_computed = False
+#            moving_average = 0    
+#    
+#    #Construction of the data matrix, a posteriori matrix and computing of the filter  
+#    if already_computed is False and nb_hist >= l+delta_t:
+#        for i in range(l):
+#            data_matrix[:,i] = history_list[i+delta_t]
+#            a_posteriori_matrix[:,i] = E_measured_list[i].flatten()
+#        F = eof.filter_training(l,m,n,alpha,data_matrix,a_posteriori_matrix,data_type='real') 
+#        already_computed = True
+#    
+#    #Estimation of E if the filter has been computed
+#    if already_computed is True:
+#        E_hat = F.dot(history_list[0])#.reshape(grid_size,grid_size)
+#        E_hat_list.insert(0,E_hat.copy())
+#        moving_average += 1
+#    
+#    
+#    img = get_intensity(current_actuators)
+#    
+#    if already_computed:
+#        
+#        E = get_electric_field(E_hat)[dark_zone]
+#        x = np.concatenate((E.real, E.imag))
+#    
+#        y = efc_matrix.dot(x)
+#        est_actuators -= efc_loop_gain * y
+#        est = get_intensity(est_actuators)
+#        
+#        plt.clf()
+#        plt.subplot(121)
+#        imshow_field(np.log10(img), vmin=-10, vmax=-5, cmap='inferno')
+#        plt.colorbar()
+#        plt.title('Without estimation')
+#        plt.draw()
+#        
+#        plt.subplot(122)
+#        imshow_field(np.log10(est), vmin=-10, vmax=-5, cmap='inferno')
+#        plt.colorbar()
+#        plt.title('With estimation')
+#        plt.draw()
+#        plt.pause(0.1)
+#    
+#    else:
+#        plt.clf()
+#        plt.subplot(121)
+#        imshow_field(np.log10(img), vmin=-10, vmax=-5, cmap='inferno')
+#        plt.colorbar()
+#        plt.title('Without estimation')
+#        plt.draw()
+#        plt.pause(0.1)
